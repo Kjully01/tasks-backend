46f5321effd6c88ec0442b4c6f87a1085f371c17
Karen Jully <91673156+Kjully01@users.noreply.github.com>
2021-12-22 18:00:40 -0300

    Update

--- migrations/20211220190148_create_table_tasks.js
+++ migrations/20211220190148_create_table_tasks.js
@@ -1,10 +1,9 @@
-
 exports.up = function(knex) {
     return knex.schema.createTable('tasks', table => {
         table.increments('id').primary()
         table.string('desc').notNull()
-        table.datetime('estimateAt').notNull()
-        table.datetime('doneAt').notNull()
+        table.datetime('estimateAt')
+        table.datetime('doneAt')
         table.integer('userId').references('id').inTable('users').notNull()
     })
 };
